e273eb422acce8c09763813ab74aa17c6f42d0b8
Marcin Pajdzik <marcin.pajdzik@codewriters.co.uk>
2012-07-26 16:48:54 +0100

    Fixed date conversion

--- js/jquery_countdown_timer_init.js
+++ js/jquery_countdown_timer_init.js
@@ -1,40 +1,20 @@
 (function($){
-    Drupal.behaviors.eu_cookie_compliance_popup = {
-      attach: function(context, settings) {
-	
-	var note = $('#jquery-countdown-timer-note'),
-		ts = new Date(Drupal.settings.jquery_countdown_timer.jquery_countdown_timer_date),
-		newYear = true;
-	
-	if((new Date()) > ts){
-		// The new year is here! Count towards something else.
-		// Notice the *1000 at the end - time must be in milliseconds
-		ts = (new Date()).getTime() + 10*24*60*60*1000;
-		newYear = false;
+  Drupal.behaviors.eu_cookie_compliance_popup = {
+    attach: function(context, settings) {
+      var note = $('#jquery-countdown-timer-note'),
+      ts = new Date(Drupal.settings.jquery_countdown_timer.jquery_countdown_timer_date * 1000);
+      alert(ts);
+      $('#jquery-countdown-timer').countdown({
+	timestamp : ts,
+	callback : function(days, hours, minutes, seconds){
+          var message = "";
+          message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+          message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+          message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+          message += seconds + " second" + ( seconds==1 ? '':'s' ) + " left";
+          note.html(message);
 	}
-		
-	$('#jquery-countdown-timer').countdown({
-		timestamp	: ts,
-		callback	: function(days, hours, minutes, seconds){
-			
-			var message = "";
-			
-			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
-			
-			if(newYear){
-				message += "left until the new year!";
-			}
-			else {
-				message += "left to 10 days from now!";
-			}
-			
-			note.html(message);
-		}
-	});
-	
-      }
+      });
     }
-  })(jQuery);
+  }
+})(jQuery);
